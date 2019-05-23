104373fcf05adb73f113d28182dc69be17164966
ClicShopping - e-commerce Solution B2B B2C <39338579+ClicShopping@users.noreply.github.com>
2019-05-23 19:38:49 -0400

    reformat

--- includes/modules/header_tags/ht_nprogress.php
+++ includes/modules/header_tags/ht_nprogress.php
@@ -1,19 +1,20 @@
 <?php
-/**
- *
- *  @copyright 2008 - https://www.clicshopping.org
- *  @Brand : ClicShopping(Tm) at Inpi all right Reserved
- *  @Licence GPL 2 & MIT
- *  @licence MIT - Portion of osCommerce 2.4
- *  @Info : https://www.clicshopping.org/forum/trademark/
- *
- */
+  /**
+   *
+   * @copyright 2008 - https://www.clicshopping.org
+   * @Brand : ClicShopping(Tm) at Inpi all right Reserved
+   * @Licence GPL 2 & MIT
+   * @licence MIT - Portion of osCommerce 2.4
+   * @Info : https://www.clicshopping.org/forum/trademark/
+   *
+   */
 
   use ClicShopping\OM\HTML;
   use ClicShopping\OM\Registry;
   use ClicShopping\OM\CLICSHOPPING;
 
-  class ht_nprogress {
+  class ht_nprogress
+  {
     public $code;
     public $group;
     public $title;
@@ -22,7 +23,8 @@
     public $enabled = false;
     public $pages;
 
-    public function __construct() {
+    public function __construct()
+    {
 
       $this->code = get_class($this);
       $this->group = basename(__DIR__);
@@ -30,14 +32,15 @@
       $this->title = CLICSHOPPING::getDef('module_header_tags_nprogress_title');
       $this->description = CLICSHOPPING::getDef('module_header_tags_nprogress_description');
 
-      if ( defined('MODULE_HEADER_NPROGRESS_STATUS') ) {
+      if (defined('MODULE_HEADER_NPROGRESS_STATUS')) {
         $this->sort_order = MODULE_HEADER_NPROGRESS_SORT_ORDER;
         $this->enabled = (MODULE_HEADER_NPROGRESS_STATUS == 'True');
         $this->pages = MODULE_HEADER_NPROGRESS_DISPLAY_PAGES;
       }
     }
 
-    public function execute() {
+    public function execute()
+    {
       $CLICSHOPPING_Template = Registry::get('Template');
 
       $footer = '<!-- Nprogress Start -->' . "\n";
@@ -50,20 +53,23 @@
 
       $CLICSHOPPING_Template->addBlock($footer, 'footer_scripts');
 
-      $nprogress = '<link rel="stylesheet" href="' . CLICSHOPPING::link('sources/javascript/nprogress/nprogress.min.css', null, false) .'">';
+      $nprogress = '<link rel="stylesheet" href="' . CLICSHOPPING::link('sources/javascript/nprogress/nprogress.min.css', null, false) . '">';
 
-      $CLICSHOPPING_Template->addBlock($nprogress, $this->group );
+      $CLICSHOPPING_Template->addBlock($nprogress, $this->group);
     }
 
-    public function isEnabled() {
+    public function isEnabled()
+    {
       return $this->enabled;
     }
 
-    public function check() {
+    public function check()
+    {
       return defined('MODULE_HEADER_NPROGRESS_STATUS');
     }
 
-    public function install() {
+    public function install()
+    {
       $CLICSHOPPING_Db = Registry::get('Db');
 
       $CLICSHOPPING_Db->save('configuration', [
@@ -109,14 +115,16 @@
 
     }
 
-    public function remove() {
+    public function remove()
+    {
       return Registry::get('Db')->exec('delete from :table_configuration where configuration_key in ("' . implode('", "', $this->keys()) . '")');
     }
 
-    public function keys() {
+    public function keys()
+    {
       return array('MODULE_HEADER_NPROGRESS_STATUS',
-                   'MODULE_HEADER_NPROGRESS_SORT_ORDER',
-                   'MODULE_HEADER_NPROGRESS_DISPLAY_PAGES'
-                  );
+        'MODULE_HEADER_NPROGRESS_SORT_ORDER',
+        'MODULE_HEADER_NPROGRESS_DISPLAY_PAGES'
+      );
     }
   }
